30fc1e97bfaf90502e5278b03925a36ccfc3b4f0
simonepesenti <tanty@libero.it>
2023-09-10 09:17:12 +0200

    init

---
+++ src/Calcola.java
@@ -0,0 +1,10 @@
+public class Calcola {
+    static int x = 2;
+    public static void scrivi(int y){
+
+        if (y < 4){
+            x++;
+        }
+        System.out.println("ora calcolo " + y + x);
+    }
+}
