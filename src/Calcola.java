19eb8cdd1c6d98e76ff385114774414d52d4bcbd
simonepesenti <tanty@libero.it>
2023-09-10 09:30:46 +0200

    modifica stampa

--- src/Calcola.java
+++ src/Calcola.java
@@ -1,10 +1,10 @@
 public class Calcola {
-    static int x = 2;
+    static int x = 20;
     public static void scrivi(int y){
 
         if (y < 4){
             x++;
         }
-        System.out.println("ora calcolo " + y + x);
+        System.out.println("ora calcolo " + y + " " + x);
     }
 }
